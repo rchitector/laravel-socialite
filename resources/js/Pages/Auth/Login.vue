8aadf8328f482ca80e3e938da17231352962c9da
rchitector <r.chitector@gmail.com>
2023-12-28 19:31:56 +0200

    laravel/socialite + github + google

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -86,5 +86,18 @@ const submit = () => {
                 </PrimaryButton>
             </div>
         </form>
+
+        <div class="mt-4">
+            <Link :href="route('auth.github.redirect')" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                Log in GitHub
+            </Link>
+        </div>
+
+        <div class="mt-4">
+            <Link :href="route('auth.google.redirect')" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                Log in Google
+            </Link>
+        </div>
+
     </AuthenticationCard>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,4 +34,41 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+});
+
+
+Route::get('/auth/github/redirect', function () {
+    $url = Socialite::driver('github')->redirect()->getTargetUrl();
+    return Inertia::location($url);
+})->name('auth.github.redirect');
+
+Route::get('/auth/github/callback', function () {
+    $githubUser = Socialite::driver('github')->stateless()->user();
+    $user = User::updateOrCreate([
+        'provider_id' => $githubUser->getId()
+    ], [
+        'name' => $githubUser->getName() ?? $githubUser->nickname,
+        'email' => $githubUser->getEmail(),
+        'provider_type' => 'github',
+    ]);
+    auth()->login($user);
+    return to_route('dashboard');
+});
+
+Route::get('/auth/google/redirect', function () {
+    $url = Socialite::driver('google')->redirect()->getTargetUrl();
+    return Inertia::location($url);
+})->name('auth.google.redirect');
+
+Route::get('/auth/google/callback', function () {
+    $googleUser = Socialite::driver('google')->stateless()->user();
+    $user = User::updateOrCreate([
+        'provider_id' => $googleUser->getId()
+    ], [
+        'name' => $googleUser->getName() ?? $googleUser->nickname,
+        'email' => $googleUser->getEmail(),
+        'provider_type' => 'google',
+    ]);
+    auth()->login($user);
+    return to_route('dashboard');
 });
